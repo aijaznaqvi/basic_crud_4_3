e97af7cd134949e0046a4535d84e9653e583e97a
Aijaz Hussain Naqvi <aijaznaqvi@hotmail.com>
2020-08-25 20:55:16 -0400

    Initial commit

---
+++ src/DataFixtures/ArticleFixture.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ArticleFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            $article = new Article();
+            $article->setName(sprintf('foo%d', $i));
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
